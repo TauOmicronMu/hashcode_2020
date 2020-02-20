0b329f8a315e87aaa4fd8f8811a7db8cd2b2474d
TauOmicronMu <tauomicronmurs@gmail.com>
2020-02-20 20:24:17 +0000

    finished(?) greedy approach

--- greedy_approach.py
+++ greedy_approach.py
@@ -1,6 +1,7 @@
 from hashcode_shared import *
 
-FILEPATH = "datasets/f_libraries_of_the_world.txt"
+# FILEPATH = "datasets/a_example.txt"
+
 
 NUM_DAYS = 0  
 NUM_BOOKS = 0
@@ -13,6 +14,8 @@ def setup():
     '''
     Loads the data from the inital file (FILEPATH)
     '''
+    global NUM_DAYS, NUM_BOOKS, NUM_LIBRARIES
+
     with open(FILEPATH, 'r') as f:
         #  Load the number of days, libraries, and books.
         line_1 = f.readline()
@@ -68,6 +71,8 @@ def heuristic(days_left, library):
         Calculates the maximum score a library will obtain with
         some number of days left
     '''
+    global BOOKS
+
     #  We have this much less time overall
     days_left -= library.signup_time     
 
@@ -76,40 +81,66 @@ def heuristic(days_left, library):
 
     #  Calculate the score by taking all of the best books. 
     #  Don't choose any that have already been scanned.
-    chosen_books = [b.score for b in library.books[:max_taken] if b in BOOKS]
-    score = sum(chosen_books)
+    chosen_books = [b for b in library.books[:max_taken] if b in BOOKS]
+    chosen_books_scores = [b.score for b in chosen_books]
+    score = sum(chosen_books_scores)
 
     return (chosen_books, score)
 
 
 def greedy_schedule(): 
+    global BOOKS
+    global LIBRARIES
+
     signup_schedule = [] 
     scanning_schedule = []
 
     #  Work through the days.
     for d in range(NUM_DAYS):
+        print("[SCHEDULING] DAY {}".format(d))
+
+        #  If there are no more libraries, quit
+        if len(LIBRARIES) == 0:
+            break
+
+        #  If no more libraries can be scheduled, we're done.
+        if max([l.signup_time for l in LIBRARIES]) > NUM_DAYS - d:
+            break
+
         #  Calculate the maximum scores for each Library
         scores = [(l, heuristic(NUM_DAYS - d, l)) for l in LIBRARIES]
 
         #  Choose the Library (and corresponding set of Books) that generate
-        #  the maximum score. 
+        #  the maximum score.
+        sorted_choices = sorted(scores, key=lambda c: c[1][1], reverse=True)
+        top_choice = sorted_choices[0]
+
+        top_library = top_choice[0]
+        top_books = top_choice[1][0]
 
         #  Add the best library to the signup_schedule
+        signup_schedule.append(top_library)
 
         #  Add the corresponding books to the scanning_schedule
+        scanning_schedule.append(top_books)
 
         #  Remove the chosen Books from BOOKS
+        for book in top_books:
+            BOOKS.remove(book)
 
         #  Remove the chosen Library from LIBRARIES
-        
+        LIBRARIES.remove(top_library)
 
     return zip(signup_schedule, scanning_schedule)  
 
 
-def output():
-    pass
+def output(schedule):
+    print("{}".format(len(schedule)))
+    for choice in schedule:
+        print("{} {}".format(choice[0].index, len(choice[1])))
+        print(" ".join([str(b.index) for b in choice[1]]))
 
 if __name__ == "__main__":
     setup() 
-    schedule = greedy_schedule()
-    output() 
+    schedule = list(greedy_schedule())
+    output(schedule) 
